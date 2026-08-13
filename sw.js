3f1e345ae187a9a4b906d280085079d2090a9970
Agustinadearagon <72619859+Agustinadearagon@users.noreply.github.com>
2026-08-13 03:00:10 +0200

    Add service worker for file caching
    Implement service worker for caching files and handling fetch requests.

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
 s
+const CACHE_NAME = "rpsls-v2";
+const archivos = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", (evento) => {
+  evento.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(archivos))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evento) => {
+  evento.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(
+        nombres.map((nombre) => {
+          if (nombre !== CACHE_NAME) return caches.delete(nombre);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((respuesta) => respuesta || fetch(evento.request))
+  );
+});
